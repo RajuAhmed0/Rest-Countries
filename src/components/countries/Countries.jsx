22a6026576b91622952c83351b66f6d7c8fdccad
Your Name <you@example.com>
2024-10-07 01:19:07 +0600

    first commit

---
+++ src/components/countries/Countries.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import "./Countries.css";
+import Country from '../country/Country';
+const Countries = () => {
+    const [countries, setCountries] = useState([])
+    const [countryName, setCountryName] = useState([])
+    const [addImg, setAddImg] = useState([])
+
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/all')
+            .then(res => res.json())
+            .then(data => setCountries(data))
+    }, [])
+
+
+
+
+    return (
+        <div>
+            <div style={{ marginBottom: '15px' }}>
+                {
+                    countryName.map(CountryN => <span style={{ marginRight: '15px', fontSize: '25px', fontWeight: '600' }}>{CountryN},</span>)
+                }
+            </div>
+        
+
+            <div className='countries-container'>
+                {
+                    countries.map(country => <Country
+                        setAddImg={setAddImg}
+                        addImg={addImg}
+
+                        setCountryName={setCountryName}
+                        countryName={countryName}
+                        key={country.cca3}
+                        country={country}
+                    ></Country>)
+                }
+            </div>
+
+        </div>
+    );
+};
+
+export default Countries;
---
+++ src/components/country/Country.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import "./Country.css";
+
+const Country = ({ country, setCountryName, countryName, setAddImg, addImg }) => {
+
+    const { name, flags } = country
+
+    const [visit, setVisit] = useState(false)
+    const [add, setAdd] = useState(false)
+    const [addFlags, setAddFlags] = useState(false)
+
+    const addCountryBtn = () => {
+        setCountryName([...countryName, name.common])
+        setAdd(true)
+    }
+    const addImgBtn = ()=>{
+        setAddImg([...addImg, flags.png])
+        setAddFlags(true)
+    }
+
+
+    return (
+        <div className='country-container'>
+            <h2>Country Name: {name.common}</h2>
+            <div className='img-container'>
+                <img src={flags.png} alt={flags.alt} />
+            </div>
+            {!visit ?
+                <button style={{ marginRight: "10px" }} onClick={() => setVisit(!visit)}>{visit ? 'Visited' : 'Visit'}</button> :
+                <button disabled>Visited</button>
+            }
+            {!add ?
+                <button onClick={() => addCountryBtn()}>Add Name</button> :
+                <button disabled>Add Name</button>
+            }
+           { !addFlags ?
+             <button onClick={() => addImgBtn()} style={{ marginLeft: '10px' }}>Add Flag</button> :
+             <button disabled>Add Flag</button>
+           }
+        </div>
+    );
+};
+
+export default Country;
